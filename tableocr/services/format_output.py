e3da1e0969302316fdfddac84e33ce22edb26395
Sanjeev Shakya <sanjeevshakya@lftechnology.com>
2020-05-06 17:10:18 +0545

    Update tableocr/services/format_output.py
    Co-authored-by: susankoju <31833796+susankoju@users.noreply.github.com>

--- tableocr/services/format_output.py
+++ tableocr/services/format_output.py
@@ -7,8 +7,8 @@ from tableocr.util.file import get_csv_filename, get_json_filename
 
 def nested_array_to_formated_file(nestedList, columns, fileFormat, filePath=''): 
     basedir = os.path.abspath(os.path.dirname(__file__))
-    if(fileFormat == 'json'):
-        if(not filePath):
+    if fileFormat == 'json':
+        if not filePath:
             filePath = os.path.join("output/json")
         jsonValue = nested_list_to_json(nestedList, columns)
         with open(get_json_filename(filePath), 'w') as outputFile:
